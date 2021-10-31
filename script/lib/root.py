2b837d3fcdbf05fb4fe649951f34ee8ccda512a4
amankh <aman.ankh@gmail.com>
2021-10-31 13:54:11 +0100

    [root] fix securetty

--- script/lib/root.py
+++ script/lib/root.py
@@ -1,6 +1,9 @@
 import subprocess
 
-#sed -e 's/^\([^#].*\)/# \1/g'
-
+# comment all TTY devices the root user is allowed to login on
 def securetty(void):
-    p = subprocess.Popen(["sed", "-e", "'s/^\([^#].*\)/# \1/g'", "/etc/securetty"])
+    p = subprocess.run(["sed", "-i", r"s/^\([^#].*\)/# \1/g", "/etc/securetty"])
+
+# Lock root account (TODO: optionnel lock account)
+def loock_root(void):
+    p = subprocess.Popen(["passwd", "-l", "root"])
